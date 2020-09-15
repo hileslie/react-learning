6805af6298df63c960f25d6bc11d81a6cd4d73bf
小蜗 <627054877@qq.com>
2020-09-15 22:18:32 +0800

    useState

---
+++ react-hooks/src/example2.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Example() {
+    const [ age, setAge] = useState(18)
+    const [ sex, setSex] = useState('男')
+    return (
+        <div>
+            <p>数组 {age} -- {sex} </p>
+            <button onClick={() => {setAge(age + 1)}}>增加</button>
+            <button onClick={() => {setSex(sex + 1)}}>增加</button>
+        </div>
+    )
+}
+
+export default Example;
